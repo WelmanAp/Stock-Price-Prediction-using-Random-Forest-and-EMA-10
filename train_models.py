2a3d7af8113b616f671db94d0d4e7e7fa544ebe7
Welman Apridius <welmanapridius942@gmail.com>
2025-01-29 21:26:14 +0700

    train_models.py

--- train_models.py
+++ train_models.py
@@ -6,23 +6,19 @@ import joblib
 def train_model(file_path):
     data = pd.read_excel(file_path)
 
-    # Gunakan 3 fitur: EMA_10, Return, dan Close
     X = data[['Close', 'EMA_10', 'Return']]
-    y = data['Close'].shift(-1).dropna()  # Target tetap 'Close' hari berikutnya
-    X = X.iloc[:-1]  # Sinkronisasi dengan target
+    y = data['Close'].shift(-1).dropna() 
+    X = X.iloc[:-1] 
 
     if len(X) < 1:
         print(f"Tidak ada data untuk pelatihan di {file_path}.")
         return
 
-    # Split data menjadi training dan testing
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    # Buat model Random Forest dan latih
     model = RandomForestRegressor(n_estimators=100, random_state=42)
     model.fit(X_train, y_train)
 
-    # Simpan model ke folder "models"
     model_name = file_path.split('/')[-1].replace('.xlsx', '_model.pkl')
     joblib.dump(model, f'models/{model_name}')
     print(f"Model untuk {file_path} telah disimpan.")
